577147c0778f9df3e8c5adeb19d67952a08f074d
milson.antonio <milson.antonio@dev.com>
2024-06-15 00:39:31 +0200

    revert (unrecorded changes)

--- src/main/java/com/milsondev/taskmanagement/controller/TaskController.java
+++ src/main/java/com/milsondev/taskmanagement/controller/TaskController.java
@@ -1,13 +1,10 @@
 package com.milsondev.taskmanagement.controller;
 
-import com.milsondev.taskmanagement.api.TaskDto;
 import com.milsondev.taskmanagement.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @Controller("/")
 public class TaskController {
@@ -22,16 +19,7 @@ public class TaskController {
     @GetMapping("/")
     public ModelAndView home() {
         ModelAndView mv = new ModelAndView("index");
-        List<TaskDto> taskDtoList = taskService.getTaskList();
-        mv.addObject("taskDtoList", taskDtoList);
         return mv;
-    }
-
-    @GetMapping("/add-new-task")
-    public ModelAndView pageNewTask() {
-        ModelAndView modelAndView = new ModelAndView("new-task");
-        return modelAndView;
-
     }
 
 }
